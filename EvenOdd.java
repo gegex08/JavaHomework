e741b0c4bf6980ac53e1aea6f50e77642ba4d9fa
Genny Ocampo <gegex08@aol.com>
2024-01-25 10:55:07 -0600

    Add files via upload
    Even Odd Homework

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evenodd;
+
+/**
+ *
+ * @author ocampog4446
+ */
+public class EvenOdd {
+    number=45;
+    if(number%2==0)
+    {
+        System.out.println("Number is Even");
+    }
+    if(number%2==1){
+            System.out.println("Number is Odd");
+    
+    
+}
