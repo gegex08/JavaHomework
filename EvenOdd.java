742b6165976f84c564f9d0f2aa18478e42d20ec8
Genny Ocampo <gegex08@aol.com>
2024-01-25 12:53:20 -0600

    Update EvenOdd.java
    public static main void

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,17 +3,19 @@ package evenodd;
  *
  * @author ocampog4446
  */
+
 public class EvenOdd {
+    public static void main(String[] args) {
     int number = 45;
 
     if(number%2==0)
-    {
+        {
         System.out.println("Number is Even");
     }
     if(number%2==1)
     {
         System.out.println("Number is Odd");
     }
-    
+    }
     
 }
