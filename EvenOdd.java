4618b3c97dd4a2151c7a565ab0d6775d3ea31245
Genny Ocampo <gegex08@aol.com>
2024-01-25 11:18:03 -0600

    Update EvenOdd.java
    Clean

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package evenodd;
-
 /**
  *
  * @author ocampog4446
  */
 public class EvenOdd {
-    number=45;
+    int number = 45;
+
     if(number%2==0)
     {
         System.out.println("Number is Even");
     }
-    if(number%2==1){
-            System.out.println("Number is Odd");
+    if(number%2==1)
+    {
+        System.out.println("Number is Odd");
+    }
     
     
 }
