dc023d3630f78bd7b11e6dd608cfea4bd045f45a
Genny Ocampo <gegex08@aol.com>
2024-01-26 09:11:51 -0600

    Add files via upload

---
+++ CalcCircumference.java
@@ -0,0 +1,55 @@
+package calccircumference;
+
+import java.util.Scanner;
+
+/****************************************************
+ * Programmer: Geneiva Ocampo
+ * Course CSCI 1471
+ * Date 01/31/2024
+ * Assignment: Homework #2: CalcCircumference
+ * Environment Java with Netbeans
+ * Files Included: CalcCircumference
+ * Purpose: To write an app that allows user too input radius and the program 
+ * calculates the diameter, circumference, and area of a circle.
+ * Input: radius
+ * Preconditions/ Assumptions: Integer data type
+ * Equations: circumference = 2*pi*radius, diameter =2 *radius, area= pi*radius^2
+ * Output: 
+ * Enter radius: 3
+`* Diameter is 6
+`* Area is 28.274334
+`* Circumference is 18.849556
+ * Postconditions/Assumptions: Type of Integer Variable 
+ * Contraints: negative numbers 
+ * Algorithm: 
+ * Prompt user to input number for the radius
+ * Calculate and output diameter, area, and circumference
+ ****************************************************/
+
+public class CalcCircumference {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Scanner class
+        Scanner input = new Scanner( System.in );
+        
+        //Ask user for radius
+        System.out.print( "Enter the radius of the circle: " );
+        
+        //Declares input and variables as double data types to give broader range
+        double radius = input.nextInt();
+        double d = 2;
+        
+        //prints diameter
+        System.out.printf("Diameter is %.0f\n", (radius * d));
+        //prints circumference formatted with 6 digits after the decimal
+        System.out.printf("Circumference is %.6f\n", (Math.PI * radius * d));
+        //prints area with 6 digit after decimal point
+        System.out.printf("Area is %.6f\n", Math.PI * radius * radius);
+
+    }
+    
+}
