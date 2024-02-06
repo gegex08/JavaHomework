2c61045592b87cf5295f37351b903f1ada867b77
Genny Ocampo <gegex08@aol.com>
2024-02-06 11:12:12 -0600

    Add files via upload

---
+++ BuildCar.java
@@ -0,0 +1,31 @@
+package buildcar;
+
+/**
+ *
+ * @author geneivaocampo
+ */
+public class BuildCar {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        private int Speed = 0;
+        private int Odometer = 0;
+        private int Direction = 0; 
+        private int Fuel_gauge = 0;
+        
+    }
+    public static void main(String[] args)
+    {
+    public void testCar1(){
+        System.out.println("Status at Finish");
+        System.out.println("");
+        System.out.println("Speed = " Speed);
+        System.out.println("");
+        System.out.println("Fuel = " Fuel);
+        System.out.println("");
+        System.out.println("Direction = " Direction);
+        System.out.println("");
+        System.out.println("Odometer = " Odometer);
+        System.out.println("I traveled" Speed/Fuel_gauge "and used" Odometer "gallons of fuel."); 
+    }
+}
