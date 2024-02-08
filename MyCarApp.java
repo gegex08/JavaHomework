6e5ada6b6832f75e773ef7879569e706ad31b940
Genny Ocampo <gegex08@aol.com>
2024-02-07 22:01:32 -0600

    Update MyCarApp.java
    more methods

--- MyCarApp.java
+++ MyCarApp.java
@@ -11,11 +11,15 @@ public class MyCarApp {
         BuildCar testCar1 = new BuildCar();
         BuildCar testCar2 = new BuildCar();
         
-        testCar1.testCar();
+        testCar1.startTest();
         testCar1.hitGas(50);
         testCar1.hitBreak(10);
         testCar1.hitGas(20);
         testCar1.addMiles(100);
+        testCar1.addMiles(20);
+        testCar1.checkFuel_gauge(10);
+        testCar1.changeDirection(90);
+        testCar1.endTest();
         
         testCar2.hitGas(50);
         testCar2.hitGas(-10);
