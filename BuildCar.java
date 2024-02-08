9362e2a9f61d9cfbbf60e540fd8d7cf532d2bb29
Genny Ocampo <gegex08@aol.com>
2024-02-07 22:55:35 -0600

    Update BuildCar.java
    Finished Homework Ass. for Car 1

--- BuildCar.java
+++ BuildCar.java
@@ -13,48 +13,40 @@ public class BuildCar {
     private int Fuel_gauge = 0;
         
     public void hitGas(int accelerate){
-        System.out.println("Speed increased by " + accelerate + " mph.");
+        System.out.println("Speed increased by " + accelerate + " mph.\n");
         Speed = Speed + accelerate;
     }
     public void hitBreak(int decrement){
-        System.out.println("Speed decreased by " + decrement + " mph.");
+        System.out.println("Speed decreased by " + decrement + " mph.\n");
         Speed = Speed - decrement;
     }
     public void changeDirection(int increment){
-        System.out.println("I just turned " + increment + " degrees.");
+        System.out.println("I just turned " + increment + " degrees.\n");
         Direction = Direction + increment;
     }
     public void addMiles(int miles){
-        System.out.println("I added " + miles + " more miles to the trip.");
+        System.out.println("I added " + miles + " more miles to the trip.\n");
         Odometer = Odometer + miles;
     }
     public void checkFuel_gauge(int gas){
-        System.out.println("Just added " + gas + " gallons of fuel."); 
-        Fuel_gauge = Odometer / 20;
+        System.out.println("Just added " + gas + " gallons of fuel.\n"); 
+        Fuel_gauge = Fuel_gauge + gas;
     }
     public void startTest(){
-        System.out.println("Status at Start");
-        System.out.println("");
-        System.out.println("Speed = 0");
-        System.out.println("");
-        System.out.println("Fuel = 0");
-        System.out.println("");
-        System.out.println("Direction = 0");
-        System.out.println("");
-        System.out.println("Odometer = 0");
+        System.out.println("Status at Start\n");
+        System.out.println("Speed = 0\n");
+        System.out.println("Fuel = 0\n");
+        System.out.println("Direction = 0\n");
+        System.out.println("Odometer = 0\n");
         
     }
     public void endTest(){
-        System.out.println("Status at Finish");
-        System.out.println("");
-        System.out.println("Speed = " + Speed);
-        System.out.println("");
-        System.out.println("Fuel = " + Fuel_gauge);
-        System.out.println("");
-        System.out.println("Direction = " + Direction);
-        System.out.println("");
-        System.out.println("Odometer = " + Odometer);
-        System.out.println("I traveled " + Odometer + " and used " + Fuel_gauge + " gallons of fuel");
+        System.out.println("Status at Finish\n");
+        System.out.println("Speed = " + Speed +"\n");
+        System.out.println("Fuel = " + Fuel_gauge +"\n");
+        System.out.println("Direction = " + Direction +"\n");
+        System.out.println("Odometer = " + Odometer +"\n");
+        System.out.printf("%s %d %s %.2f %s" , "I traveled " , Odometer , " and used " , Odometer/20.00 , " gallons of fuel\n");
     }
 }
 
