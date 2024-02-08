2c8321d512e2990a138ec658600fcba2d71ae2ea
Genny Ocampo <gegex08@aol.com>
2024-02-07 22:27:50 -0600

    Update BuildCar.java
    Added more test variables

--- BuildCar.java
+++ BuildCar.java
@@ -21,29 +21,40 @@ public class BuildCar {
         Speed = Speed - decrement;
     }
     public void changeDirection(int increment){
-
         System.out.println("I just turned " + increment + " degrees.");
+        Direction = Direction + increment;
     }
     public void addMiles(int miles){
         System.out.println("I added " + miles + " more miles to the trip.");
+        Odometer = Odometer + miles;
     }
     public void checkFuel_gauge(int gas){
-        System.out.println("Just added " + gas + " gallons of fuel.");  
+        System.out.println("Just added " + gas + " gallons of fuel."); 
+        Fuel_gauge = Odometer / 20;
     }
     public void startTest(){
-    System.out.println("Status at Start");
-    System.out.println("");
-    System.out.println("Speed = 0");
-    System.out.println("");
-    System.out.println("Fuel = 0");
-    System.out.println("");
-    System.out.println("Direction = 0");
-    System.out.println("");
-    System.out.println("Odometer = 0");
+        System.out.println("Status at Start");
+        System.out.println("");
+        System.out.println("Speed = 0");
+        System.out.println("");
+        System.out.println("Fuel = 0");
+        System.out.println("");
+        System.out.println("Direction = 0");
+        System.out.println("");
+        System.out.println("Odometer = 0");
         
     }
     public void endTest(){
-        System.out.println(Speed);
+        System.out.println("Status at Finish");
+        System.out.println("");
+        System.out.println("Speed = " + Speed);
+        System.out.println("");
+        System.out.println("Fuel = " + Fuel_gauge);
+        System.out.println("");
+        System.out.println("Direction = " + Direction);
+        System.out.println("");
+        System.out.println("Odometer = " + Odometer);
+        System.out.println("I traveled " + Odometer + " and used " + Fuel_gauge + " gallons of fuel");
     }
 }
 
